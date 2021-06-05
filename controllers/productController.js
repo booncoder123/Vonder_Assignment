be7d0e29ccba2f9ec6dfa3939065e9564d954c50
chanok <Chanok_boon@hotmail.com>
2021-06-05 20:13:58 +0700

    [ADD] UPDATE AND DELETE
    insert update and delete function in controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -59,3 +59,48 @@ exports.findProducts =  async (req,res,next) => {
         res.setEncoding(err)
     }
 }
+
+exports.deleteProductById =  async (req,res,next) => {
+    try{
+        const id_candy = req.params.id
+        console.log(req.params.id)
+
+        
+        const candy = await Candy.findByIdAndDelete({_id : id_candy})
+        res.status(200).json({ message: "Candy fetched!", candy: candy });
+    }
+    catch(err){
+        res.setEncoding(err)
+    }
+}
+
+exports.updateProducts = async (req,res,next) => {
+    console.log("POST")
+    try{
+
+        const id_candy = req.params.id
+
+        const title = req.body.title
+        const price = req.body.price
+        const owner = req.body.owner
+        const pack = req.body.package
+        
+
+       
+        const new_candy = new Candy({
+            title : title,
+            price : price,
+            owner : owner,
+            package: pack
+          });
+    
+          const  dy = await Candy.findOneAndDelete({_id : id_candy},new_candy)
+          res.status(200).json({ message: "Candy updated!", candy: new_candy });
+        
+        console.log("Candy yum!")
+
+    } catch(err){
+        console.log("err: ",err)
+        res.send(err)
+    }
+}
--- routes/admin.js
+++ routes/admin.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const  productController = require('../controllers/productController')
 
 router.post('/add',productController.postProducts)
+router.delete('/delete/candy/:id',productController.deleteProductById)
+router.put('/update/candy/:id',productController.updateProducts)
 module.exports = router
