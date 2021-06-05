d90b1d310ecdb0db45309254caf844b990fd70fd
chanok <Chanok_boon@hotmail.com>
2021-06-05 20:13:58 +0700

    [ADD] 🍗UPDATE AND DELETE
    insert update and delete function in controller

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ const bodyParser = require('body-parser')
 const errorRoutes = require('./controllers/errorController');
 const mongoConnect = require('./utils/database')
 const admin = require('./routes/admin')
-const app =express()
 const shopRoute = require('./routes/shop')
+const app =express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(shopRoute)
 app.use('/admin',admin)
 app.use(errorRoutes.errorPage)
+
+
+
 mongoConnect()
 app.listen(3000)
 
--- routes/shop.js
+++ routes/shop.js
@@ -4,4 +4,5 @@ const  productController = require('../controllers/productController')
 
 router.get('/candys',productController.getProducts)
 router.get('/candys/:title',productController.findProducts )
+
 module.exports = router
